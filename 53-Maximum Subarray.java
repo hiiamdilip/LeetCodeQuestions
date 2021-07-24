4f43a64acd57e69c1be1a56dedf55240f468c3b0
Dilip Kumar <dilip35kumar@gmail.com>
2021-07-24 17:08:21 +0530

    Create 53-Maximum Subarray.java

---
+++ 53-Maximum Subarray.java
@@ -0,0 +1,25 @@
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+// A subarray is a contiguous part of an array.
+
+// Example 1:
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+// Example 2:
+
+// Input: nums = [1]
+// Output: 1
+  
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currSum = nums[0];
+        for(int i = 1;i<nums.length;i++ ){
+            currSum = (currSum < 0) ? nums[i] : (currSum + nums[i]);
+            maxSum = (currSum > maxSum) ? currSum : maxSum;
+        }
+        return maxSum;
+    }
+}
