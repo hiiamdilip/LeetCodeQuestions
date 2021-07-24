f6ea42da0f30a6ddead67e26797ded12c942575a
Dilip Kumar <dilip35kumar@gmail.com>
2021-07-24 17:18:01 +0530

    Create 172-Factorial Trailing Zeroes.java

---
+++ 172-Factorial Trailing Zeroes.java
@@ -0,0 +1,24 @@
+// Given an integer n, return the number of trailing zeroes in n!.
+
+// Follow up: Could you write a solution that works in logarithmic time complexity?
+
+// Example 1:
+
+// Input: n = 3
+// Output: 0
+// Explanation: 3! = 6, no trailing zero.
+// Example 2:
+
+// Input: n = 5
+// Output: 1
+// Explanation: 5! = 120, one trailing zero.
+
+class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        for(int i =5; n/i>=1; i *= 5){
+            count += n/i;
+        }
+        return count;
+    }
+}
