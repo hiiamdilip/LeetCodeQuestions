45eaa872fa1fd64ad35986a1f4064262c4f214cf
Dilip Kumar <dilip35kumar@gmail.com>
2021-07-29 19:06:37 +0530

    Update and rename 349. Intersection of Two Arrays.java to 349-350. Intersection of Two Arrays.java

--- Arrays/349-350. Intersection of Two Arrays.java
+++ Arrays/349-350. Intersection of Two Arrays.java
@@ -73,3 +73,43 @@ class Solution {
         return result;
     }
 }
+
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+// Example 1:
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2,2]
+// Example 2:
+
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [4,9]
+// Explanation: [9,4] is also accepted.
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int l1 = nums1.length;
+        int l2 = nums2.length;
+        int l = (l1 > l2) ? l2 : l1 ; 
+        int[] res = new int[l];
+        int i=0,j=0,k = 0;
+        while(i<l1 && j<l2){
+            if(nums1[i] == nums2[j]){
+                res[k] = nums1[i];
+                i++;
+                j++;
+                k++;
+            }
+            else{
+                if(nums1[i] < nums2[j]) i++;
+                else j++;
+            }
+        }
+        int[] result = new int[k];
+        for(int m=0;m<k;m++){
+            result[m] = res[m];
+        }
+        return result;
+    }
+}
