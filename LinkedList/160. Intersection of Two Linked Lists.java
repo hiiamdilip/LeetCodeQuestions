8dce8aa6355c71c1b65990b6aec1407f15ae7f9f
Dilip Kumar <dilip35kumar@gmail.com>
2021-07-29 21:56:33 +0530

    Create 160. Intersection of Two Linked Lists.java

---
+++ LinkedList/160. Intersection of Two Linked Lists.java
@@ -0,0 +1,30 @@
+// Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+// Output: Intersected at '8'
+// Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+// From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
+        ListNode currentA = headA;
+        ListNode currentB = headB;
+        while(currentA != currentB){
+           if(currentA == null) currentA = headB;
+           else currentA = currentA.next;
+           if(currentB == null) currentB = headA;
+           else currentB = currentB.next;
+        }  
+        return currentA;
+    }
+}
