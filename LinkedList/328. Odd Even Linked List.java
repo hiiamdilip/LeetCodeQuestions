661ebd3d5fd518c1efe17f916b9c49f37155d3f1
Dilip Kumar <dilip35kumar@gmail.com>
2021-07-31 15:01:32 +0530

    Create 328. Odd Even Linked List.java

---
+++ LinkedList/328. Odd Even Linked List.java
@@ -0,0 +1,32 @@
+// Input: head = [1,2,3,4,5]
+// Output: [1,3,5,2,4]
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null) return null;
+        
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+      
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        
+        odd.next = evenHead;
+        return head;
+    }
+}
