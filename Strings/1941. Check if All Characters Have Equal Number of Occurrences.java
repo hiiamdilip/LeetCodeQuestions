703394d0c28c91750bb2ed7746f6ac19cb5ff626
Dilip Kumar <dilip35kumar@gmail.com>
2021-08-05 21:26:17 +0530

    Update 1941. Check if All Characters Have Equal Number of Occurrences.java

--- Strings/1941. Check if All Characters Have Equal Number of Occurrences.java
+++ Strings/1941. Check if All Characters Have Equal Number of Occurrences.java
@@ -33,3 +33,19 @@ class Solution {
         else return true;
     }
 }
+
+//Method 2:
+class Solution {
+    public boolean areOccurrencesEqual(String s) {
+        int[] freq = new int[26];
+        
+        for(int i = 0; i < s.length(); i++){
+            freq[s.charAt(i) - 'a']++;
+        }
+        Arrays.sort(freq);
+        for(int i = 24; i > 0 && freq[i] > 0; i--){
+            if(freq[i] != freq[i+1]) return false;
+        }
+        return true;
+    }
+}
