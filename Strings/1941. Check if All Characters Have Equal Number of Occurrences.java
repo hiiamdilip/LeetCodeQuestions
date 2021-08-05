5c2862a771986e0627e8db79f21d9d9d02bd12fd
Dilip Kumar <dilip35kumar@gmail.com>
2021-08-05 20:39:41 +0530

    Create 1941. Check if All Characters Have Equal Number of Occurrences.java

---
+++ Strings/1941. Check if All Characters Have Equal Number of Occurrences.java
@@ -0,0 +1,35 @@
+// Given a string s, return true if s is a good string, or false otherwise.
+
+// A string s is good if all the characters that appear in s have the same number of occurrences (i.e., the same frequency).
+
+// Example 1:
+
+// Input: s = "abacbc"
+// Output: true
+// Explanation: The characters that appear in s are 'a', 'b', and 'c'. All characters occur 2 times in s.
+// Example 2:
+
+// Input: s = "aaabb"
+// Output: false
+// Explanation: The characters that appear in s are 'a' and 'b'.
+// 'a' occurs 3 times while 'b' occurs 2 times, which is not the same number of times.
+
+//Method 1:
+class Solution {
+    public boolean areOccurrencesEqual(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        for(int i =0; i< s.length(); i++){
+            if(map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+            }else{
+                map.put(s.charAt(i), 1);
+            }
+        }
+        for(Map.Entry<Character, Integer> entry : map.entrySet()){
+            set.add(entry.getValue());
+        }
+        if(set.size() > 1) return false;
+        else return true;
+    }
+}
