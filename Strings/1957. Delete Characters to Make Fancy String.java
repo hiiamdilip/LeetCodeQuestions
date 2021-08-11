663d3dc42a96a7b255873ac833dd6b28a3abe768
Dilip Kumar <dilip35kumar@gmail.com>
2021-08-11 17:05:54 +0530

    Update 1957. Delete Characters to Make Fancy String.java

--- Strings/1957. Delete Characters to Make Fancy String.java
+++ Strings/1957. Delete Characters to Make Fancy String.java
@@ -24,3 +24,16 @@
 // Input: s = "aab"
 // Output: "aab"
 // Explanation: No three consecutive characters are equal, so return "aab".
+
+class Solution {
+    public String makeFancyString(String s) {
+        if (s.length() < 3) return s;
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.substring(0,2));
+        for(int i = 2; i < s.length(); i++){
+            if(s.charAt(i-2) == s.charAt(i-1) && s.charAt(i-1) == s.charAt(i)) continue;
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
