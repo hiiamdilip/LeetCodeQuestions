3060cf5f2aa41d6757470033a66b690a96c04435
Dilip Kumar <dilip35kumar@gmail.com>
2021-08-28 10:05:33 +0530

    Update 145. Binary Tree Postorder Traversal.java

--- Trees/145. Binary Tree Postorder Traversal.java
+++ Trees/145. Binary Tree Postorder Traversal.java
@@ -28,3 +28,25 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        s1.push(root);
+        
+        while( !s1.isEmpty()){
+           root = s1.pop();
+           s2.push(root);
+           if(root.left != null)s1.push(root.left);
+           if(root.right != null)s1.push(root.right);
+        }
+        while( !s2.isEmpty()){
+           list.add(s2.pop().val); 
+        }
+        return list;
+    }
+}
