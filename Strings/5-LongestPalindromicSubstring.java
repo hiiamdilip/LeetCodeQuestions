2f00e8ea1c813126cc0acf7d5c83a8adaafcc09b
Dilip Kumar <dilip35kumar@gmail.com>
2021-08-31 07:15:31 +0530

    Update 5-LongestPalindromicSubstring.java

--- Strings/5-LongestPalindromicSubstring.java
+++ Strings/5-LongestPalindromicSubstring.java
@@ -29,3 +29,35 @@ private int expandAroundCenter(String s, int left, int right) {
     return right - left - 1;
 }
 }
+
+//This is JavaBrains solution and i follow this approach;
+class Solution {
+    
+    int resultStart;
+    int resultLength;
+    
+    public String longestPalindrome(String s) {
+        int strLength = s.length();
+        
+        if(strLength < 2)return s;
+        
+        for(int start = 0; start < strLength-1; start++){
+            expandRange(s, start, start);
+            expandRange(s, start, start+1);
+        }  
+        
+        return s.substring(resultStart, resultStart + resultLength);  
+    }
+    
+    public void expandRange(String s, int start, int end){
+        while(start>=0 && end <s.length() && s.charAt(start) == s.charAt(end)){
+            start--;
+            end++;
+        }
+        
+        if(resultLength < end - start -1){
+           resultStart = start +1;
+           resultLength = end - start -1;
+        }
+    }
+}
