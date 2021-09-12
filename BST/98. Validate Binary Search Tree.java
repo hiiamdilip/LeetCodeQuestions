19b9bc61a6c5fab5ea90b50b5fe418a8a327f2d0
Dilip Kumar <dilip35kumar@gmail.com>
2021-09-12 08:03:35 +0530

    Create 98. Validate Binary Search Tree.java

---
+++ BST/98. Validate Binary Search Tree.java
@@ -0,0 +1,60 @@
+// Input: root = [2,1,3]
+// Output: true
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        double smallest = - Double.MAX_VALUE;
+        
+        while(!stack.isEmpty() || root != null){
+           if(root != null) {
+              stack.push(root); 
+              root = root.left;
+           } else{
+              root = stack.pop();
+              if(root.val <= smallest) return false;
+              smallest = root.val;
+              root = root.right;
+           }
+        }
+        return true;
+    }
+}
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        while(!stack.isEmpty() || root != null){
+           if(root != null) {
+              stack.push(root); 
+              root = root.left;
+           } else{
+              root = stack.pop();
+              list.add(root.val);
+              root = root.right;
+           }
+        }
+        
+        for(int i = 1; i< list.size(); i++){
+            if(list.get(i-1) >= list.get(i)) return false;
+        }
+        return true;
+    }
+}
