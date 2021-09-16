fb1bb34234552518d609e3ee370d3ea4d53ae09c
Dilip Kumar <dilip35kumar@gmail.com>
2021-09-16 08:59:19 +0530

    Update 146. LRU Cache.java

--- Arrays/146. LRU Cache.java
+++ Arrays/146. LRU Cache.java
@@ -33,3 +33,93 @@ class LRUCache {
         }
     }
 }
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+class LRUCache {
+    private Map<Integer, ListNode> hashTable = new HashMap<>();
+    
+    ListNode head;
+    ListNode tail;
+    
+    int capacity;
+    int totalItemsInCache;
+    
+    public LRUCache(int capacity) {
+       this.capacity = capacity;
+       totalItemsInCache = 0; 
+        
+        head = new ListNode();
+        tail = new ListNode();
+        
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public class ListNode{
+        int key;
+        int value;
+        
+        ListNode prev;
+        ListNode next;
+    }
+    
+    public int get(int key) {
+        ListNode node = hashTable.get(key);
+        
+        if(node == null) return -1;
+        
+        moveToHead(node);
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+        ListNode node = hashTable.get(key);
+        
+        if(node == null){
+            ListNode newNode = new ListNode();
+            newNode.value = value;
+            newNode.key = key;
+            
+            hashTable.put(key, newNode);
+            addToFront(newNode);
+            totalItemsInCache++;
+            
+            if(totalItemsInCache > capacity) removeLRUEntry();
+        } else{
+            node.value = value;
+            moveToHead(node);
+        }
+    }
+    
+    private void removeLRUEntry(){
+        ListNode tailItem = tail.prev;
+        removeFromList(tailItem);
+        
+        hashTable.remove(tailItem.key);
+        --totalItemsInCache;
+    }
+    
+    private void removeFromList(ListNode node) {
+        ListNode savedPrev = node.prev;
+        ListNode savedNext = node.next;
+
+        savedPrev.next = savedNext;
+        savedNext.prev = savedPrev;
+    }
+    
+    private void moveToHead(ListNode node) {
+        removeFromList(node);
+        addToFront(node);
+    }
+    
+    private void addToFront(ListNode node) {
+        node.prev = head;
+        node.next = head.next;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+}
