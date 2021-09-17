ca6c0e643f9ea5d03ebbecff5c5df9131912fbfb
Dilip Kumar <dilip35kumar@gmail.com>
2021-09-17 11:27:32 +0530

    Create 232. Implement Queue using Stacks.java

---
+++ Arrays/232. Implement Queue using Stacks.java
@@ -0,0 +1,32 @@
+class MyQueue {
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+    
+    public MyQueue() {
+       this.stack1 = new Stack<>();
+       this.stack2 = new Stack<>(); 
+    }
+    
+    public void push(int x) {
+        stack1.push(x);
+    }
+    
+    public int pop() {
+       if(stack2.isEmpty()) 
+           while(!stack1.isEmpty())
+               stack2.push(stack1.pop());
+        return stack2.pop();
+    }
+    
+    public int peek() {
+        if(stack2.isEmpty()) 
+           while(!stack1.isEmpty())
+               stack2.push(stack1.pop());
+        return stack2.peek();
+    }
+    
+    public boolean empty() {
+        if(stack1.isEmpty() && stack2.isEmpty()) return true;
+        return false;
+    }
+}
