10fc892b2da9aaef9225cd443d3cbf5322695d83
Dilip Kumar <dilip35kumar@gmail.com>
2021-09-26 12:34:05 +0530

    Update Leaf nodes of tree.java

--- Trees/Leaf nodes of tree.java
+++ Trees/Leaf nodes of tree.java
@@ -19,9 +19,9 @@ public static void printLeafNodesIteratively(TreeNode root)
 		while (!stack.isEmpty()) 
 		{ 
 			TreeNode node = stack.pop(); 
+			if (node.left == null && node.right == null) result.add(node.value);
 			if (node.right != null) stack.add(node.right); 
 			if (node.left != null) stack.add(node.left);
-			if (node.left == null && node.right == null) result.add(node.value);
 		}
 	}
 
