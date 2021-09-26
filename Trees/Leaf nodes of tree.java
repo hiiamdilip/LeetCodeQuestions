cd86b671ddb5b15496cc041705ab9b3c44c43c80
Dilip Kumar <dilip35kumar@gmail.com>
2021-09-26 12:33:25 +0530

    Create Leaf nodes of tree.java

---
+++ Trees/Leaf nodes of tree.java
@@ -0,0 +1,29 @@
+public static void printLeafNodes(TreeNode node) 
+{ 
+      List<Integer> result = new ArrayList<>();
+  
+      if (node == null) return null;
+      if (node.left == null && node.right == null) result.add(node.value);
+      printLeafNodes(node.left); 
+      printLeafNodes(node.right);
+      return result;
+}
+
+public static void printLeafNodesIteratively(TreeNode root) 
+	{ 
+		if (root == null) return null; 
+		Stack<TreeNode> stack = new Stack<>(); 
+		stack.push(root); 
+		List<Integer> result = new ArrayList<>();
+		
+		while (!stack.isEmpty()) 
+		{ 
+			TreeNode node = stack.pop(); 
+			if (node.right != null) stack.add(node.right); 
+			if (node.left != null) stack.add(node.left);
+			if (node.left == null && node.right == null) result.add(node.value);
+		}
+	}
+
+
+
