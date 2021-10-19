62a6a0a562ce08e582ee6c886c9b39a8842f99a8
Dilip Kumar <dilip35kumar@gmail.com>
2021-10-19 09:21:48 +0530

    Update and rename Column Title from COlumn Number.java to 171. Excel Sheet Column Number.java

--- Strings/171. Excel Sheet Column Number.java
+++ Strings/171. Excel Sheet Column Number.java
@@ -17,3 +17,16 @@ public static void printString(int columnNumber)
         }
         System.out.println(columnName.reverse());
     }
+
+
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int result = 0;
+        for (int i = 0; i < columnTitle.length(); i++)
+        {
+            result = result * 26;
+            result = result + columnTitle.charAt(i) - 'A' + 1;
+        }
+        return result;
+    }
+}
