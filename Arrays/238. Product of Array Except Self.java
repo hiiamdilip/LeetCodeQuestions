ea761b59858e6e4a29b85dcfc8c8e8708f04fd37
Dilip Kumar <dilip35kumar@gmail.com>
2026-01-02 22:26:26 +0530

    Update 238. Product of Array Except Self.java

--- Arrays/238. Product of Array Except Self.java
+++ Arrays/238. Product of Array Except Self.java
@@ -36,3 +36,25 @@ class Solution {
         
     }
 }
+
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] ans = new int[nums.length];
+        Arrays.fill(ans, 1);
+
+        int curr = 1;
+        for(int i =0; i<nums.length; i++){
+            ans[i]*=curr;
+            curr*=nums[i];
+        }
+
+        curr = 1;
+        for(int i = nums.length -1; i>=0; i--){
+            ans[i]*=curr;
+            curr*=nums[i];
+        }
+
+        return ans;
+    }
+}
