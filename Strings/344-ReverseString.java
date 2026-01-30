6a35c45c7ac3938c0c1a3e38ea2786f7f9a04b42
Dilip Kumar <dilip35kumar@gmail.com>
2026-01-30 17:58:14 +0530

    Update 344-ReverseString.java

--- Strings/344-ReverseString.java
+++ Strings/344-ReverseString.java
@@ -17,3 +17,23 @@ class Solution {
         }  
     }
 }
+
+Using recursion:
+  
+class Solution {
+    public void reverseString(char[] s) {
+        reverse(s, 0, s.length-1);
+    }
+
+    public void reverse(char[] s, int left, int right){
+        if(left >= right){
+            return;
+        }
+
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        reverse(s, left +1 , right -1);
+    }
+}
