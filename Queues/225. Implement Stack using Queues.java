f631b73bf202020ea93c95e44990c2247c3f9c6a
Dilip Kumar <dilip35kumar@gmail.com>
2026-02-15 20:01:03 +0530

    Update 225. Implement Stack using Queues.java

--- Queues/225. Implement Stack using Queues.java
+++ Queues/225. Implement Stack using Queues.java
@@ -36,3 +36,37 @@ class MyStack {
         return queue1.isEmpty();
     }
 }
+
+// Solution 2
+// Interview preference, using one queue but push costly. pop and peek faster o(1)
+
+class MyStack {
+
+    private Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new ArrayDeque<>();
+    }
+    
+    public void push(int x) {
+        queue.offer(x);
+
+        int rotations = queue.size() -1;
+
+        for(int i =0; i<rotations; i++){
+            queue.offer(queue.poll());
+        }
+    }
+    
+    public int pop() {
+        return queue.poll();
+    }
+    
+    public int top() {
+        return queue.peek();
+    }
+    
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
